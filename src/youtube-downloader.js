9cb233e2889a34891f172db0b5e4e1871859c769
TulioAbreu <marco-tulioabreu1@hotmail.com>
2019-12-08 11:51:35 -0300

    Check if URL is null before downloading

--- src/youtube-downloader.js
+++ src/youtube-downloader.js
@@ -1,11 +1,17 @@
 const ytdl = require('ytdl-core')
+const fs = require('fs')
 
 module.exports = class YoutubeDownloader {
     constructor() {
 
     }
 
-    async downloadMp4Stream(URL) {
-        return await ytdl(URL, { filter: 'audioonly', format: 'mp4'})
+    async downloadMp4Stream(URL, name) {
+        if (URL) {
+            return await ytdl(URL, { 
+                filter: 'audioonly', 
+                format: 'mp4'
+            }).pipe(fs.createWriteStream('./songlist/'+name+'.mp4'))
+        }
     }
 }
