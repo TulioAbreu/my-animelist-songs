984306ca2f87be61beb34de97d4b4081c339a122
TulioAbreu <marco-tulioabreu1@hotmail.com>
2019-12-10 13:53:22 -0300

    Check if user page exists

--- src/mal-profile-scrap.js
+++ src/mal-profile-scrap.js
@@ -3,7 +3,12 @@ const getPageHtml = require('./common')
 module.exports = async function getMalProfileAnimes(username) {
     let animes = []
     const htmlPage = await getPageHtml(`http://myanimelist.net/animelist/${username}`)
-    let raw = htmlPage.match(/anime_id&quot;:(.*?),/g)
+
+    if (htmlPage.indexOf('Could not find user') >= 0) {
+        return null
+    }
+
+    const raw = htmlPage.match(/anime_id&quot;:(.*?),/g)
     for (let i = 0; i < raw.length; ++i) {
         let rawLink = raw[i].split('&quot;:')[1].slice(0, -1);
         animes.push(rawLink)
