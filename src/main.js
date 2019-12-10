b85e98939660bf7a031a555cfc36c3d226605e1b
TulioAbreu <marco-tulioabreu1@hotmail.com>
2019-12-10 13:53:47 -0300

    Refactor main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 const YoutubeSearcher = require('./youtube-searcher')
-const YoutubeDownloader = require('./youtube-downloader')
 const scrapProfile = require('./mal-profile-scrap')
 const getAnimeSongs = require('./mal-anime-scrap')
 const sleep = require('system-sleep')
 const cliProgress = require('cli-progress')
-const ffmpeg = require('ffmpeg')
-const bar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic)
-const config = require('../config.js')
+const fs = require('fs')
 
+const config = require('../config.js')
 async function main() {
     function wait() {
         sleep(1000)
@@ -31,66 +29,75 @@ async function main() {
         return name
     }
     
+    function getUrlFromId(id) {
+        return `https://myanimelist.net/anime/${id}`
+    }
+
     async function getAnimesSongs(animeUrlList) {
-        if (animeUrlList.length <= 0) { 
+        if (animeUrlList.length === 0) { 
             return []
         }
         
         let currAnimeURL = animeUrlList[0]
         const currAnimeSongList = await getAnimeSongs(currAnimeURL)
+
         wait()  // Be nice to the website!
         
-        console.log(currAnimeSongList)
-
         return currAnimeSongList.concat(await getAnimesSongs(animeUrlList.slice(1)))
+    }
+
+    async function getAnimesUrls(searchStringsList, ytSearcher) {
+        if (searchStringsList.length === 0) {
+            return []
+        }
+
+        const searchString = searchStringsList[0]
+        const sanitizedSearchString = sanitizeMusicName(searchString)
+
+        const song = {
+            name: sanitizedSearchString,
+            URL: await ytSearcher.search(sanitizedSearchString)
+        }
+
+        wait()  // Be nice to the website!
+
+        if (! song.URL) {
+            return await getAnimesUrls(searchStringsList.slice(1), ytSearcher)
+        }
+        else {
+            return [song].concat(await getAnimesUrls(searchStringsList.slice(1), ytSearcher))
+        }
+    }
+
+    if (config.profileName == "") {
+        console.log("[ERROR] No profile name specified on config.js file.")
+        return
     }
 
     console.log(`Collecting profile data [profileName="${config.profileName}"]`)
     const animeIdList = await scrapProfile(config.profileName)
 
-    const animeUrlList = []
-    for (let i = 0; i < animeIdList.length; ++i) {
-        animeUrlList.push(`https://myanimelist.net/anime/${animeIdList[i]}`)
+    if (! animeIdList) {
+        console.log('[ERROR] Invalid MyAnimeList profile.')
+        return
     }
+
+    let animeUrlList = []
+    animeIdList.forEach(id => {
+        animeUrlList.push(getUrlFromId(id))
+    })
 
     console.log('Collecting anime songs')
-    let songList = await getAnimesSongs(animeUrlList)
+    const songList = await getAnimesSongs(animeUrlList)
 
     console.log('Collecting songs URLs')
-    let ytSearcher = new YoutubeSearcher()
-    let ytSongList = []
-    let getAllUrls = async function(x) {
-        if (x < songList.length) {
-            console.log(`Requesting song ${x}/${songList.length}`)
-            let currentSong = songList[x]
-            let sanitizedSong = sanitizeMusicName(currentSong)
-        
-            let newSong = {
-                name: sanitizedSong,
-                URL: await ytSearcher.search(sanitizedSong)
-            }
+    const ytSongList = await getAnimesUrls(songList, new YoutubeSearcher())
 
-            if (newSong.URL) {
-                ytSongList.push(newSong)
-                console.log(ytSongList[ytSongList.length-1])
-            }
-            await getAllUrls(x+1)
-        }
-    }
-    await getAllUrls(0)
-
-    console.log('Downloading songs')
-    let ytDownloader = new YoutubeDownloader()
-    let downloadAllSongs = async function(x) {
-        if (x < ytSongList.length) {
-            console.log(`Downloading song ${x}/${ytSongList.length}`)
-            let { name, URL } = ytSongList[x]
-            ytDownloader.downloadMp4Stream(URL, name).then(_ => {
-                downloadAllSongs(x+1)
-            })
-        }
-    }
-    await downloadAllSongs(0)
+    fs.writeFile(
+        `./${config.profileName}_list.json`, 
+        JSON.stringify({songList: ytSongList}, 0, 4), 
+        function(err) {if (err) console.error(err)}
+    )
 }
 
 main()
